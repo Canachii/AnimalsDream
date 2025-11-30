d7464f6bc29259131e4719dda77fba98ad4f0b27
gudals123 <104682516+gudals123@users.noreply.github.com>
2025-11-30 21:53:13 +0900

    플레이어 이동, 점프 구현
    #설명
    1. WASD 입력시 카메라 기준 앞뒤좌우 방향으로 이동(WSAD 입력시에만 캐릭터 회전)
    2. Spacebar 입력시 점프 기능 구현

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,26 +4,86 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Vector2 inputVec2;
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    
+    [Header ("Movement")]
+    public float moveSpeed = 5f;
+    public float jumpHeight = 5f;
+    public float rotationSpeed = 10f;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("Camera")]
+    [SerializeField] private Transform camTransform;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = GetCameraRelativeMoveDirection();
+
+        if (move.sqrMagnitude > 1f)
+            move.Normalize();
+
+        if (move.sqrMagnitude > 0.0001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(move, Vector3.up);
+
+            Quaternion newRot = Quaternion.Slerp(
+                rb.rotation,
+                targetRot,
+                rotationSpeed * Time.fixedDeltaTime
+            );
+            rb.MoveRotation(newRot);
+        }
+
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+
+        isGrounded = Physics.Raycast(transform.position + Vector3.up * 0.1f,
+                                 Vector3.down,
+                                 groundCheckDistance,
+                                 groundMask);
+    }
+
+
+    private Vector3 GetCameraRelativeMoveDirection()
+    {
+        Vector3 forward = camTransform.forward;
+        Vector3 right = camTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * inputVec2.y + right * inputVec2.x;
+    }
 
     public void OnMove(InputValue value)
     {
         inputVec2 = value.Get<Vector2>();
-        Debug.Log(inputVec2);
     }
 
     public void OnJump(InputValue value)
     {
-        Debug.Log("Jump!");
+        if(isGrounded)
+        {
+            //rb.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+            Vector3 v = rb.linearVelocity;
+            v.y = jumpHeight;
+            rb.linearVelocity = v;
+            isGrounded = false;
+        }
     }
 
     public void OnUseSkill(InputValue value)
     {
-        Debug.Log("Skill!");
+        Debug.Log("UseSkill");
     }
 
-    void Update()
-    {
-        Vector3 move = new Vector3(inputVec2.x, 0f, inputVec2.y);
-        transform.Translate(move * 5f * Time.deltaTime);
-    }
 }
