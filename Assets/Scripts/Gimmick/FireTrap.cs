7f20290cc5dfe0c7cc77232141b1a4e9c5f0fa3b
flslin <ych40250@gmail.com>
2025-12-02 16:47:36 +0900

    TrapController추가
    트랩 발동 시 로그출력

---
+++ Assets/Scripts/Gimmick/FireTrap.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FireTrap : TrapController
+{
+    protected override void OnActivate()
+    {
+
+    }
+
+    protected override void OnDeactivate()
+    {
+
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+}
