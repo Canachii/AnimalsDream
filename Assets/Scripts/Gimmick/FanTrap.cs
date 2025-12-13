2c45ec8b927e1668c1a8da3cc307a6b01cb765bf
hjhj1197 <hjhj122812@gmail.com>
2025-12-14 01:32:49 +0900

    Revert "Merge pull request #19 from Canachii/playerDetect"
    This reverts commit b0a6c9e83fc088bff4622ced383633ba43d737e8, reversing
    changes made to 6972a856d105aedc3ec74dfedbbcd7fb808b178b.

--- Assets/Scripts/Gimmick/FanTrap.cs
+++ Assets/Scripts/Gimmick/FanTrap.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public class FanTrap : TrapController
 {
-    [Header("Knockback")]
-    [SerializeField] private float knockbackForce = 20f;
-    [SerializeField] private float upForce = 5f;
-
-    private string target = "Player";
-
     protected override void OnActivate()
     {
 
@@ -21,25 +15,5 @@ public class FanTrap : TrapController
     protected override void Start()
     {
         base.Start();
-    }
-
-    protected override void OnTriggerStay(Collider other)
-    {
-        if (isActive && other.CompareTag(target))
-        {
-            Rigidbody rb = other.attachedRigidbody;
-            if (rb == null) return;
-
-            Debug.Log($"{gameObject.name}함정 발동");
-
-            Vector3 dir = transform.forward;
-            dir.y = 0f;
-            dir = dir.normalized;
-
-            Vector3 forceDir = dir * knockbackForce + Vector3.up * upForce;
-            rb.AddForce(forceDir);
-
-            Debug.Log("넉백 적용");
-        }
     }
 }
