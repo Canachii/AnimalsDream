5ff913e8f5bb845e62bc176fe69e58137402b35f
flslin <ych40250@gmail.com>
2025-12-21 21:22:26 +0900

    오타 수정
    오타 수정

--- Assets/Scripts/Gimmick/DisappearTrap.cs
+++ Assets/Scripts/Gimmick/DisappearTrap.cs
@@ -1,35 +1,10 @@
 using System.Collections;
 using UnityEngine;
 
-public class DisapperaTrap : TrapController
+public class DisappearTrap : TriggerTrapController
 {
-
-    protected override void OnActivate()
+    protected override void OnTrapTriggered(Collider other)
     {
 
-    }
-
-    protected override void OnDeactivate()
-    {
-
-    }
-
-    protected override void Start()
-    {
-        base.Start();
-        originColor = trapRenderer.material.color;
-    }
-
-    protected override void OnTriggerEnter(Collider other)
-    {
-        
-    }
-
-    private IEnumerator Disappear()
-    {
-        if (trapRenderer != null)
-            trapRenderer.material.color = Color.red;
-
-        yield return new WaitForSeconds(1f);
     }
 }
