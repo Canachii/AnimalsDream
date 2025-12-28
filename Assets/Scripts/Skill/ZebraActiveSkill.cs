1b0e058d94b3b17d6e03409bbd50006320ebc4fa
gudals123 <104682516+gudals123@users.noreply.github.com>
2025-12-28 15:57:23 +0900

    실드 마테리얼 추가

---
+++ Assets/Scripts/Skill/ZebraActiveSkill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ZebraActiveSkill : Skill
+{
+    protected override void OnUse(PlayerController user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Skill/ZebraPsssiveSkill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ZebraPsssiveSkill : Skill
+{
+    protected override void OnUse(PlayerController user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
