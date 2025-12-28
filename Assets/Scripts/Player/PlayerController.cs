8224548b7ce8bc31c434c376b216846b31b7cf0b
hjhj1197 <hjhj122812@gmail.com>
2025-12-29 01:41:58 +0900

    벌레 스킬에 필요한거 추가(cc기)
    그냥 멈추게 하는거면 걍 무브먼트에 추가하면 되는데, CC기는 점프나 스킬같은거도 다 못 쓰게 해야해서 여기다 추가함

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -12,6 +13,9 @@ public class PlayerController : NetworkBehaviour
     private PlayerMovement movement;
     private PlayerSkillController skillController;
     private PlayerInputHandler input;
+
+    // cc skill (for beetle)
+    private bool isCrowdControlled = false;
 
     private void Awake()
     {
@@ -30,6 +34,11 @@ public class PlayerController : NetworkBehaviour
     {
         if (!IsOwner) return; // 내 캐릭터가 아니면 아래 로직 실행 안함
 
+        if (isCrowdControlled)
+        {
+            movement.SetMoveInput(Vector2.zero); // CC
+            return; // CC 걸리면 점프, 스킬 실행 안하고 리턴
+        }
         movement.SetMoveInput(input.MoveInput);
 
         if (input.JumpPressed)
@@ -37,5 +46,23 @@ public class PlayerController : NetworkBehaviour
 
         if (input.SkillPressed)
             skillController.UseSkill(0);
+    }
+    // [추가] 외부(스킬)에서 호출할 함수
+    public void ApplyCrowdControl(float duration)
+    {
+        // 코루틴 충돌 방지를 위해 기존 것 끄고 새로 시작
+        StopCoroutine("CrowdControlRoutine");
+        StartCoroutine(CrowdControlRoutine(duration));
+    }
+
+    private IEnumerator CrowdControlRoutine(float duration)
+    {
+        isCrowdControlled = true;
+        // Debug.Log($"[{name}] 행동 불가 ({duration}초)");
+
+        yield return new WaitForSeconds(duration);
+
+        isCrowdControlled = false;
+        // Debug.Log($"[{name}] 풀려남.");
     }
 }
